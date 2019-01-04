d40b22cb4ab669c97a432775eb9fc7411aa9ee54
Jinyan <jinyan.shen21@gmail.com>
2019-01-04 10:41:23 -0500

    minor fixes

--- server/index.js
+++ server/index.js
@@ -15,36 +15,30 @@ module.exports = app
 if (process.env.NODE_ENV !== 'production') require('../secrets')
 
 
-const createApp = () => {
-  // logging middleware
-  app.use(morgan('dev'))
+// logging middleware
+app.use(morgan('dev'))
 
-  // body parsing middleware
-  app.use(express.json())
-  app.use(express.urlencoded({extended: true}))
+// body parsing middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-  // static file-serving middleware
-  app.use(express.static(path.join(__dirname, '..', 'public')))
+// static file-serving middleware
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
-  // sends index.html
-  app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-  })
+// sends index.html
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
 
-  // error handling endware
-  app.use((err, req, res, next) => {
-    console.error(err)
-    console.error(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error.')
-  })
-}
+app.use('/api', require('./api'))
 
-const startListening = () => {
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
 
-  app.listen(PORT, () =>
-    console.log(`Listening on port ${PORT}`)
-  )
-}
-
-createApp()
-startListening()
+app.listen(PORT, () =>
+console.log(`Listening on port ${PORT}`)
+)
